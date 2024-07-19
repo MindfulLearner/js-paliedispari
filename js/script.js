7eb0227c1105a0f405b3f41b5ca84184c308958d
MindfulLearner <dimajosh1009@gmail.com>
2024-07-19 19:55:21 +0200

    swagger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // SCRIPTING JS SWAG
 
+// consideriamo che se prendiamo un numero qualsiasi e invertiamo ogni volta esempiop
+// 56 e facciamo 56 + 65 = otterremo 121 che anch'essa numero palindromo!
 
 
-
+// 
+//     function easyCipher(a) {
+//     let b = -4;
+//     let c = '';
+//     for (let d = 0; d < a.length; d++) {
+//         let e = a[d];
+//         if (e.match(/[a-z]/i)) {
+//             let f = a.charCodeAt(d);
+//             if (f >= 65 && f <= 90)
+//                 e = String.fromCharCode((f - 65 + b) % 26 + 65);
+//             else if (f >= 97 && f <= 122)
+//                 e = String.fromCharCode((f - 97 + b) % 26 + 97);
+//         }
+//         c += e;
+//     }
+//     return c;
+// }
+// 
+// 
+// console.log(easyCipher("wipsvmwspzmwimkecdwadaw"));
